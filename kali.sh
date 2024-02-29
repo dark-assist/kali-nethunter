882e66c3956e0402ca8d87436040a1f07d86c278
D4rk_4ssi$T <108276894+dark-assist@users.noreply.github.com>
2024-02-29 09:22:17 +0530

    kali.sh

--- kali.sh
+++ kali.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+clear
 figlet "KALI" | lolcat
 echo "installation script by sanatani-hackers"
 echo "telegram:- https://t.me/temuxhacking"
+sleep 3
 apt update && yes | apt upgrade
-
+sleep 5
